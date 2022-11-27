e36e8cad7dd822d56cfebb0d43c912e19bca6d8c
TheDkBr <icarosantana734@gmail.com>
2022-11-26 22:25:29 -0300

    Create pagamento.js

---
+++ pagamento.js
@@ -0,0 +1,28 @@
+pagamento () {
+    var method
+    window.document.getElementById()
+
+    switch {
+        case {
+            if (
+                method = dinheiro;
+            ) 
+        }
+        case {
+            if (
+                method = pix;
+            )
+        }
+        case {
+            if (
+                method = debito;
+            )
+        }
+        case {
+            if (
+                method = credito;
+            )
+        }
+
+        }
+    }
