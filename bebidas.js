bbba9fe447ffa223dcaa5d6e83e9c58c42451b69
Vinícius Braga <vinib2013@gmail.com>
2022-11-27 19:29:27 -0300

    Update bebidas.js

--- bebidas.js
+++ bebidas.js
@@ -5,11 +5,12 @@ function additem(id) {
     var qtd = Number(document.getElementById(`qtd_${id}`).value)
     var valor = parseFloat(document.getElementById(`price_${id}`).value).toFixed(2)
 
-    //if(qtd.value == true){
-    console.log(itens)
-    itens.push({"nome": nome, "qtd": qtd, "valor": valor})
+    if(qtd == 0)
+        alert("Informe a quantidade, por favor")
+    else{
+        console.log(itens)
+        itens.push({"nome": nome, "qtd": qtd, "valor": valor})
 
-    localStorage.setItem("itens", JSON.stringify(itens))
-    //}
-    //else alert("Informe a quantidade, por favor")
+        localStorage.setItem("itens", JSON.stringify(itens))
+    }
 }
