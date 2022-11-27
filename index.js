0cad87e8fd543be3d59556836f6122e153db7ba3
Vinícius Braga <vinib2013@gmail.com>
2022-11-27 01:08:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,35 @@ function entrar() {
             alert("Nome não informado!")
         }else{
             Nome.style.borderColor = "white"
-            window.location.href = "cardapio.html?name=" + nome
+            window.location.href = "cardapio.html" //"cardapio.html?name=" + nome
+            localStorage.setItem("nome", nome)
         }
 }
 
-function navegar(){
-    window.location.href = "cardapio.html?name=" + nome
+function additem(){
+    //var tabela = document.getElementById("carrinho").querySelector("tbody")
+
+    var nome = document.getElementsByClassName("card-title").value
+    var qtd = document.getElementById("qtd").value
+    var valor = Number(document.getElementById("price").value)
+    //var tabela = document.getElementById("carrinho")
+
+    var coluna1 = document.createElement('td')
+    var coluna2 = document.createElement('td')
+    var coluna3 = document.createElement('td')
+    var coluna4 = document.createElement('td')
+    var linha = document.createElement('tr')
+
+    coluna1.innerHTML = `${nome}`
+    coluna2.innerHTML = `${qtd}`
+    coluna3.innerHTML = `R$ ${valor.toFixed(2).replace('.', ',')}`
+    coluna4.innerHTML = 'remover'
+
+    linha.appendChild(coluna1)
+    linha.appendChild(coluna2)
+    linha.appendChild(coluna3)
+    linha.appendChild(coluna4)
+    tabela.appendChild(linha)
+
+    document.getElementById("price").value = valor * qtd
 }
