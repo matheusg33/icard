d880f766adbe74f536ee2a105dba678149069037
Vinícius Braga <vinib2013@gmail.com>
2022-11-27 19:29:35 -0300

    Update carrinho.js

--- carrinho.js
+++ carrinho.js
@@ -5,9 +5,10 @@ function exec_card(){
     console.log(itens)
     var tabela = document.getElementById("carrinho")
     var total_price = 0
+
     itens.forEach(element => {
         pula_tbl_carrinho(tabela, element.nome, element.qtd, element.valor)
-        total_price = parseFloat(total_price) + parseFloat(element.valor) 
+        total_price = parseFloat(total_price) + parseFloat(element.valor)
     });
 
     document.getElementById("total_price").innerHTML = `R$${total_price.toFixed(2)}`
@@ -21,9 +22,11 @@ function pula_tbl_carrinho(tabela, nome, qtd, valor) {
     var coluna4 = document.createElement('td')
     var linha = document.createElement('tr')
 
+    valor = valor * qtd
+
     coluna1.innerHTML = `${nome}`
     coluna2.innerHTML = `${qtd}`
-    coluna3.innerHTML = `R$ ${valor}`
+    coluna3.innerHTML = `R$ ${valor.toFixed(2)}`
     coluna4.innerHTML = 'remover'
 
     linha.appendChild(coluna1)
@@ -31,8 +34,6 @@ function pula_tbl_carrinho(tabela, nome, qtd, valor) {
     linha.appendChild(coluna3)
     linha.appendChild(coluna4)
     tabela.appendChild(linha)
-
-    //document.getElementById("price").value = valor * qtd
 }
 
 
