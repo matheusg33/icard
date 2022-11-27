3309365042d716cf38216395a77794ba7f77fce8
Vinícius Braga <vinib2013@gmail.com>
2022-11-27 18:36:30 -0300

    Update cardapio.js

--- cardapio.js
+++ cardapio.js
@@ -6,15 +6,13 @@ function additem(id) {
     var qtd = Number(document.getElementById(`qtd_${id}`).value)
     var valor = parseFloat(document.getElementById(`price_${id}`).value).toFixed(2)
 
-    if(qtd.value == true){
+    //if(qtd =! true){
         console.log(itens)
         itens.push({"nome": nome, "qtd": qtd, "valor": valor})
 
         localStorage.setItem("itens", JSON.stringify(itens))
-    } else{
-        //qtd.style.borderColor = "red"
-        alert("Informe a quantidade, por favor")
-    }
+    //}
+    //else alert("Informe a quantidade, por favor")
 }
 
 
