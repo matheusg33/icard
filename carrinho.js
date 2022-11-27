f4327be7a74a5075379b2b8dc687958edbf27f69
Vinícius Braga <vinib2013@gmail.com>
2022-11-27 18:30:06 -0300

    Create carrinho.js

---
+++ carrinho.js
@@ -0,0 +1,41 @@
+function exec_card(){
+    
+    console.log("executando auto function")
+    var itens = JSON.parse(localStorage.getItem("itens"))
+    console.log(itens)
+    var tabela = document.getElementById("carrinho")
+    var total_price = 0
+    itens.forEach(element => {
+        pula_tbl_carrinho(tabela, element.nome, element.qtd, element.valor)
+        total_price = parseFloat(total_price) + parseFloat(element.valor) 
+    });
+
+    document.getElementById("total_price").innerHTML = `R$${total_price.toFixed(2)}`
+}
+
+function pula_tbl_carrinho(tabela, nome, qtd, valor) {
+
+    var coluna1 = document.createElement('td')
+    var coluna2 = document.createElement('td')
+    var coluna3 = document.createElement('td')
+    var coluna4 = document.createElement('td')
+    var linha = document.createElement('tr')
+
+    coluna1.innerHTML = `${nome}`
+    coluna2.innerHTML = `${qtd}`
+    coluna3.innerHTML = `R$ ${valor}`
+    coluna4.innerHTML = 'remover'
+
+    linha.appendChild(coluna1)
+    linha.appendChild(coluna2)
+    linha.appendChild(coluna3)
+    linha.appendChild(coluna4)
+    tabela.appendChild(linha)
+
+    //document.getElementById("price").value = valor * qtd
+}
+
+
+
+exec_card() //sempre ultima linha
+
