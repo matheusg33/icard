7bbb65786798b13bbf24ca47f3af2b7de278f0d4
Vinícius Braga <vinib2013@gmail.com>
2022-11-27 16:13:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,30 +11,41 @@ function entrar() {
         }
 }
 
-function additem(){
+function additem() {
     //var tabela = document.getElementById("carrinho").querySelector("tbody")
 
     var nome = document.getElementsByClassName("card-title").value
-    var qtd = document.getElementById("qtd").value
+    var qtd = Number(document.getElementById("qtd").value)
     var valor = Number(document.getElementById("price").value)
-    //var tabela = document.getElementById("carrinho")
+    var tabela = document.getElementById("carrinho")
 
-    var coluna1 = document.createElement('td')
-    var coluna2 = document.createElement('td')
-    var coluna3 = document.createElement('td')
-    var coluna4 = document.createElement('td')
-    var linha = document.createElement('tr')
+    if(qtd.value =! 0){
 
-    coluna1.innerHTML = `${nome}`
-    coluna2.innerHTML = `${qtd}`
-    coluna3.innerHTML = `R$ ${valor.toFixed(2).replace('.', ',')}`
-    coluna4.innerHTML = 'remover'
+        //localStorage.setItem("produto", nome)
+        //localStorage.setItem("qtd", qtd)
 
-    linha.appendChild(coluna1)
-    linha.appendChild(coluna2)
-    linha.appendChild(coluna3)
-    linha.appendChild(coluna4)
-    tabela.appendChild(linha)
 
-    document.getElementById("price").value = valor * qtd
+        var coluna1 = document.createElement('td')
+        var coluna2 = document.createElement('td')
+        var coluna3 = document.createElement('td')
+        var coluna4 = document.createElement('td')
+        var linha = document.createElement('tr')
+
+        coluna1.innerHTML = `${nome}`
+        coluna2.innerHTML = `${qtd}`
+        coluna3.innerHTML = `R$ ${valor.toFixed(2).replace('.', ',')}`
+        coluna4.innerHTML = 'remover'
+
+        linha.appendChild(coluna1)
+        linha.appendChild(coluna2)
+        linha.appendChild(coluna3)
+        linha.appendChild(coluna4)
+        tabela.appendChild(linha)
+
+        document.getElementById("price").value = valor * qtd
+    }
+    else {
+        qtd.style.borderColor = "red"
+        alert("Nome não informado!")
+    }
 }
