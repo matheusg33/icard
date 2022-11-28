c6933a97501480ca40fb56cbeca8fce13dc99929
Vinícius Braga <vinib2013@gmail.com>
2022-11-28 16:48:48 -0300

    Update cardapio.js

--- cardapio.js
+++ cardapio.js
@@ -5,21 +5,27 @@ function additem(id) {
     var nome = document.getElementById(`prato_${id}`).innerHTML
     var qtd = Number(document.getElementById(`qtd_${id}`).value)
     var valor = parseFloat(document.getElementById(`price_${id}`).value).toFixed(2)
+    var block = false
 
     if(qtd <= 0)
         alert("Informe a quantidade, por favor")
     else{
-        console.log(itens)
-        itens.push({"nome": nome, "qtd": qtd, "valor": valor})
-
-        localStorage.setItem("itens", JSON.stringify(itens))
+        itens.forEach(element => {
+            if(element.nome == nome){
+                alert("produto já adicionado no carrinho")
+                block = true
+            }
+        })
+        if(block != true){
+            itens.push({"nome": nome, "qtd": qtd, "valor": valor})
+            localStorage.setItem("itens", JSON.stringify(itens))
+        }
     }
 }
 
 function finalizar_ped(){
     window.location.href = "carrinho.html"
 }
-
 
 
 
