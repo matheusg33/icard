31828e0487881e6bd4a0ca6494a6f0f25302e05f
Vinícius Braga <vinib2013@gmail.com>
2022-11-28 17:53:11 -0300

    Update pagamento.js

--- pagamento.js
+++ pagamento.js
@@ -1,11 +1,21 @@
-function pagamento (forma_pagmt) {
-    console.log(forma_pagmt)
+function pagamento (forma_pagmt){
     var dinheiro = window.document.getElementById("dinheiro").value
     var pix = window.document.getElementById("pix").value
     var debito = window.document.getElementById("debito").value
     var credito = window.document.getElementById("credito").value
-    console.log(text)
 
     window.location.href = "pagamento2.html?forma_pagmt" + forma_pagmt
 }
 
+function exec_card(){
+
+    var itens = JSON.parse(localStorage.getItem("itens"))
+    var total_price = 0
+
+    itens.forEach(element => {
+        total_price = parseFloat(total_price) + (parseFloat(element.valor) * (element.qtd))
+    });
+    document.getElementById("copy").innerHTML = `R$${total_price.toFixed(2)}`
+}
+
+exec_card()
